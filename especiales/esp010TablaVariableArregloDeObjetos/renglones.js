9a74dada83b5e960cccb837be83ff3886fbdaa5e
Sebastián Jofré <sjofre7@hotmail.com>
2025-09-21 01:43:36 -0300

    Add files via upload

---
+++ especiales/esp010TablaVariableArregloDeObjetos/renglones.js
@@ -0,0 +1,169 @@
+const textoRenglones = `{
+  "renglones": [
+    {
+      "NroFactura": 1001,
+      "Cod_articulo": "A001",
+      "Descripcion": "Remera basica blanca",
+      "Categoria": "Torso Superior",
+      "UnidadDeMedida": "UN",
+      "Cantidad": 2,
+      "Precio_Unitario": 32990.00,
+      "Importe_Renglon": 65980.00,
+      "FechaAlta": "20250115"
+    },
+    {
+      "NroFactura": 1001,
+      "Cod_articulo": "A002",
+      "Descripcion": "Pantalón jean clasico",
+      "Categoria": "Torso Inferior",
+      "UnidadDeMedida": "UN",
+      "Cantidad": 1,
+      "Precio_Unitario": 39999.00,
+      "Importe_Renglon": 39999.00,
+      "FechaAlta": "20250220"
+    },
+    {
+      "NroFactura": 1002,
+      "Cod_articulo": "A003",
+      "Descripcion": "Campera de abrigo",
+      "Categoria": "Torso Superior",
+      "UnidadDeMedida": "UN",
+      "Cantidad": 1,
+      "Precio_Unitario": 68586.00,
+      "Importe_Renglon": 68586.00,
+      "FechaAlta": "20250310"
+    },
+    {
+      "NroFactura": 1002,
+      "Cod_articulo": "A004",
+      "Descripcion": "Botas negras",
+      "Categoria": "Calzado",
+      "UnidadDeMedida": "PAR",
+      "Cantidad": 1,
+      "Precio_Unitario": 119999.00,
+      "Importe_Renglon": 119999.00,
+      "FechaAlta": "20250405"
+    },
+    {
+      "NroFactura": 1002,
+      "Cod_articulo": "A005",
+      "Descripcion": "Gorra gris",
+      "Categoria": "Torso Superior",
+      "UnidadDeMedida": "UN",
+      "Cantidad": 1,
+      "Precio_Unitario": 34990.00,
+      "Importe_Renglon": 34990.00,
+      "FechaAlta": "20250522"
+    },
+    {
+      "NroFactura": 1003,
+      "Cod_articulo": "A006",
+      "Descripcion": "Camisa formal azul",
+      "Categoria": "Torso Superior",
+      "UnidadDeMedida": "UN",
+      "Cantidad": 2,
+      "Precio_Unitario": 42990.00,
+      "Importe_Renglon": 85980.00,
+      "FechaAlta": "20250618"
+    },
+    {
+      "NroFactura": 1003,
+      "Cod_articulo": "A007",
+      "Descripcion": "Zapatos de vestir marrones",
+      "Categoria": "Calzado",
+      "UnidadDeMedida": "PAR",
+      "Cantidad": 1,
+      "Precio_Unitario": 98985.00,
+      "Importe_Renglon": 98985.00,
+      "FechaAlta": "20250708"
+    },
+    {
+      "NroFactura": 1004,
+      "Cod_articulo": "A008",
+      "Descripcion": "Sweater de lana merino",
+      "Categoria": "Torso Superior",
+      "UnidadDeMedida": "UN",
+      "Cantidad": 3,
+      "Precio_Unitario": 51990.00,
+      "Importe_Renglon": 155970.00,
+      "FechaAlta": "20250825"
+    },
+    {
+      "NroFactura": 1004,
+      "Cod_articulo": "A009",
+      "Descripcion": "Pollera larga negra",
+      "Categoria": "Torso Inferior",
+      "UnidadDeMedida": "UN",
+      "Cantidad": 2,
+      "Precio_Unitario": 43990.00,
+      "Importe_Renglon": 87980.00,
+      "FechaAlta": "20250912"
+    },
+    {
+      "NroFactura": 1004,
+      "Cod_articulo": "A010",
+      "Descripcion": "Zapatillas deportivas",
+      "Categoria": "Calzado",
+      "UnidadDeMedida": "PAR",
+      "Cantidad": 1,
+      "Precio_Unitario": 35990.00,
+      "Importe_Renglon": 35990.00,
+      "FechaAlta": "20251030"
+    },
+    {
+      "NroFactura": 1005,
+      "Cod_articulo": "A011",
+      "Descripcion": "Chaleco",
+      "Categoria": "Torso Superior",
+      "UnidadDeMedida": "UN",
+      "Cantidad": 1,
+      "Precio_Unitario": 32990.00,
+      "Importe_Renglon": 32990.00,
+      "FechaAlta": "20251103"
+    },
+    {
+      "NroFactura": 1005,
+      "Cod_articulo": "A012",
+      "Descripcion": "Pantalón cargo",
+      "Categoria": "Torso Inferior",
+      "UnidadDeMedida": "UN",
+      "Cantidad": 2,
+      "Precio_Unitario": 44990.00,
+      "Importe_Renglon": 89980.00,
+      "FechaAlta": "20251215"
+    },
+    {
+      "NroFactura": 1005,
+      "Cod_articulo": "A013",
+      "Descripcion": "Mocasines de cuero",
+      "Categoria": "Calzado",
+      "UnidadDeMedida": "PAR",
+      "Cantidad": 1,
+      "Precio_Unitario": 35008.00,
+      "Importe_Renglon": 35008.00,
+      "FechaAlta": "20250128"
+    },
+    {
+      "NroFactura": 1003,
+      "Cod_articulo": "A014",
+      "Descripcion": "Cinturón de cuero",
+      "Categoria": "Torso Inferior",
+      "UnidadDeMedida": "UN",
+      "Cantidad": 1,
+      "Precio_Unitario": 10000.00,
+      "Importe_Renglon": 10000.00,
+      "FechaAlta": "20250214"
+    },
+    {
+      "NroFactura": 1001,
+      "Cod_articulo": "A015",
+      "Descripcion": "Medias de algodón",
+      "Categoria": "Calzado",
+      "UnidadDeMedida": "PAR",
+      "Cantidad": 3,
+      "Precio_Unitario": 5000.00,
+      "Importe_Renglon": 15000.00,
+      "FechaAlta": "20250307"
+    }
+  ]
+}`;
---
+++ json/json/facturas.js
@@ -0,0 +1,44 @@
+const textoFacturas = `{
+  "facturas": [
+    {
+      "NroFactura": 1001,
+      "Fecha": "2025-09-15",
+      "Cliente": "Juan Pérez",
+      "CUIT": "20-12345678-9",
+      "CondicionVenta": "Contado",
+      "Total": 105979.00
+    },
+    {
+      "NroFactura": 1002,
+      "Fecha": "2025-09-15",
+      "Cliente": "María Gómez",
+      "CUIT": "27-98765432-1",
+      "CondicionVenta": "Cuenta Corriente",
+      "Total": 223575.00
+    },
+    {
+      "NroFactura": 1003,
+      "Fecha": "2025-09-16",
+      "Cliente": "Carlos Rodríguez",
+      "CUIT": "20-44556677-8",
+      "CondicionVenta": "Contado",
+      "Total": 184965.00
+    },
+    {
+      "NroFactura": 1004,
+      "Fecha": "2025-09-17",
+      "Cliente": "Laura Fernández",
+      "CUIT": "27-11223344-5",
+      "CondicionVenta": "Cuenta Corriente",
+      "Total": 279940.00
+    },
+    {
+      "NroFactura": 1005,
+      "Fecha": "2025-09-18",
+      "Cliente": "Diego Morales",
+      "CUIT": "23-99887766-4",
+      "CondicionVenta": "Contado",
+      "Total": 157978.00
+    }
+  ]
+}`;
